507a455c7610a054ffc96b51afecd7e571227800
Hariharan2729 <hariharanchandran2729@gmail.com>
2026-02-01 13:07:17 +0530

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,27 @@
+package week1day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String name = "John Doe";
+		byte age = 30;
+		float Salary = 55000.75f;
+		char Gender = 'M';
+		boolean acceptTerms = true;
+		float Rating = 4.5f;
+		long MobileNumber = 9876543210L;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		System.out.println("Name : "+name
+				+"\n"+"age : "+age
+				+"\n"+"Salary : "+Salary
+				+"\n"+"Gender : "+Gender
+				+"\n"+"AcceptTerms : "+acceptTerms
+				+"\n"+"Rating : "+Rating
+				+"\n"+"Mobile Number : "+MobileNumber
+				+"\n"+"Pin Code : "+pinCode
+				+"\n"+"Lucky Number : "+luckyNumber);
+			
+	}
+
+}
